6a9e7b6c80df5476b432ecbc1239609417be9221
Jorge <jorgengarriga@gmail.com>
2021-01-30 23:36:43 +0100

    Some extras

--- react-redux/src/App.tsx
+++ react-redux/src/App.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
-
+import * as React from 'react'
+// import React, {useState} from 'react'
+const { useEffect, useState } = React
 function App() {
+
+  const [state, setState] = useState('')
+
+  useEffect(() => {
+      fetchData().then(response => setState(response))
+  }, [])
+
   return (
     <>
-    <h1>Hello World!</h1>
+      {state && (
+        <>
+          <p>{typeof state}</p>
+          <pre>{JSON.stringify(state, null, 2)}</pre>
+        </>
+      )}
     </>
   );
+}
+
+const fetchData = async () => {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon/drednaw')
+  const pokemon = await response.json()
+  return pokemon
 }
 
 export default App;
